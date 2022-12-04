416cd2966c56ba173410fc2c8a93ef61331943cd
Benoit Jeaurond <benoit@jeaurond.dev>
2022-12-04 16:38:26 -0500

    chore: move dep top of file

--- day3/src/lib.rs
+++ day3/src/lib.rs
@@ -1,6 +1,5 @@
-use std::collections::HashSet;
-
 use itertools::Itertools;
+use std::collections::HashSet;
 
 pub fn read_file_vec(filename: &str) -> Result<String, Box<dyn std::error::Error>> {
     let path: std::path::PathBuf = [env!("CARGO_MANIFEST_DIR"), filename].into_iter().collect();
